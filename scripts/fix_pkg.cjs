d33e3a8253dda36abc1e20993a08afc6a4c78eae
BoBao <bogiabao2006@gmail.com>
2026-06-17 19:40:28 -0700

    feat: Add voice speak capability + edge-tts dependency
    - VoiceChannel.js: Added speak() function using edge-tts for TTS
    - VoiceChannel.js: joinChannel now plays greeting after joining
    - package.json: Added edge-tts dependency
    - Bot can now speak in voice channels on VPS

--- scripts/fix_pkg.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('package.json', 'utf8');
-c = c.replace(/^\uFEFF/, '');
-c = c.replace(
-  '"@discordjs/voice": "^0.19.2",',
-  '"@discordjs/voice": "^0.19.2",\n    "edge-tts": "^6.1.0",'
-);
-fs.writeFileSync('package.json', c, 'utf8');
-console.log('OK: edge-tts added to package.json');
